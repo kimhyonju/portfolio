4cbcc85001f113c1566f5989815634637f8800dc
Minoru Sakamoto <p2020640@gmail.com>
2020-03-14 19:37:17 +0900

    cloudfrontのterraformを追加

--- tf/main.tf
+++ tf/main.tf
@@ -38,3 +38,68 @@ resource "aws_s3_bucket_policy" "kimhyonju-portfolio" {
     }
 POLICY
 }
+
+locals {
+  s3_origin_id = "S3-kimhyonju-portfolio"
+}
+resource "aws_cloudfront_distribution" "kimhyonju-portfolio_distribution" {
+  aliases                        = []
+  enabled                        = true
+  http_version                   = "http2"
+  is_ipv6_enabled                = true
+  price_class                    = "PriceClass_All"
+  retain_on_delete               = false
+  tags                           = {}
+  wait_for_deployment            = true
+
+  default_cache_behavior {
+    allowed_methods = [
+      "GET",
+      "HEAD",
+    ]
+    cached_methods = [
+      "GET",
+      "HEAD",
+    ]
+    compress               = false
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${local.s3_origin_id}"
+    trusted_signers        = []
+    viewer_protocol_policy = "allow-all"
+
+    forwarded_values {
+      headers                 = []
+      query_string            = false
+      query_string_cache_keys = []
+
+      cookies {
+        forward           = "none"
+        whitelisted_names = []
+      }
+    }
+  }
+
+  origin {
+    domain_name = "${aws_s3_bucket.kimhyonju-portfolio.bucket_domain_name}"
+    origin_id   = "${local.s3_origin_id}"
+
+    s3_origin_config {
+      origin_access_identity = "origin-access-identity/cloudfront/E2TQMNU2GUNNZ4"
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      locations        = []
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    minimum_protocol_version       = "TLSv1"
+  }
+}
