f042a7c5de4ccce2c2bbb48dc2ed33053ad7ea1a
Minoru Sakamoto <p2020640@gmail.com>
2021-01-02 20:05:10 +0900

    s3を暗号化

--- tf/main.tf
+++ tf/main.tf
@@ -11,6 +11,13 @@ resource "aws_s3_bucket" "kimhyonju-portfolio" {
   bucket        = "kimhyonju-portfolio"
   force_destroy = false
   region        = "ap-northeast-1"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
   tags = {
     "Service" = "kimhyonju-portfolio"
   }
